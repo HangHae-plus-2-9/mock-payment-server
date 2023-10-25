e53326a07cc7e13a8a16614738976aa8f43fcef6
HC-kang <weston0713@gmail.com>
2023-10-25 13:52:42 +0900

    Feat: add basic APIs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
+type PaymentInfo = {
+  amount: number;
+  cardNumber: string;
+  cardHolder: string;
+  expirationDate: string;
+  securityCode: string;
+};
+
+let IS_SERVER_AVAILABLE = true;
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('/status')
+  checkServerStatus(): string {
+    return IS_SERVER_AVAILABLE ? 'Server is available' : 'Server is down';
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/payment')
+  async payment(@Body() paymentInfo: PaymentInfo): Promise<any> {
+    console.log(paymentInfo);
+
+    // Simulate some delay
+    await someDelay(jitter(500));
+    return {
+      status: 'success',
+      message: 'Payment is successful',
+      data: {
+        amount: paymentInfo.amount,
+        cardNumber: paymentInfo.cardNumber,
+        cardHolder: paymentInfo.cardHolder,
+        expirationDate: paymentInfo.expirationDate,
+        securityCode: paymentInfo.securityCode,
+      },
+    };
+  }
+
+  @Post('/down')
+  down(): string {
+    IS_SERVER_AVAILABLE = false;
+    return 'Server is down';
+  }
+
+  @Post('/up')
+  up(): string {
+    IS_SERVER_AVAILABLE = true;
+    return 'Server is up';
   }
 }
+
+const someDelay = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const jitter = (ms: number): number => {
+  return ms + 300 * (Math.random() - 0.5);
+};
